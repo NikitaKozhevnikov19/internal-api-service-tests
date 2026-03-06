bf89d2f3b418015afba4cc42e129a5bc752714f9
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-03-06 21:30:27 +0500

    refactor: add response specification for consistent json validation

--- src/test/java/api/specs/InternalSpecs.java
+++ src/test/java/api/specs/InternalSpecs.java
@@ -1,9 +1,11 @@
 package api.specs;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import static api.helpers.CustomApiListener.withCustomTemplates; // Импортируем твой листенер
+import io.restassured.specification.ResponseSpecification;
+import static api.helpers.CustomApiListener.withCustomTemplates;
 
 public class InternalSpecs {
     public static RequestSpecification requestSpec = new RequestSpecBuilder()
@@ -11,5 +13,10 @@ public class InternalSpecs {
             .setContentType(ContentType.URLENC)
             .addHeader("X-Api-Key", "qazWSXedc")
             .addFilter(withCustomTemplates())
+            .build();
+
+
+    public static ResponseSpecification responseSpec = new ResponseSpecBuilder()
+            .expectContentType(ContentType.JSON)
             .build();
 }
