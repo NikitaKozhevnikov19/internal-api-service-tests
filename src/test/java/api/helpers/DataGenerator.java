6e1345f5350347b6f0c1e47fcd8ddad0c5f9ef8f
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-03-06 21:29:55 +0500

    refactor: add allure attachment for generated tokens

--- src/test/java/api/helpers/DataGenerator.java
+++ src/test/java/api/helpers/DataGenerator.java
@@ -1,9 +1,13 @@
 package api.helpers;
 
+import io.qameta.allure.Attachment;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class DataGenerator {
+
+    @Attachment("Сгенерированный тестовый токен")
     public static String generateToken() {
-        return RandomStringUtils.random(32, "0123456789ABCDEF");
+        String token = RandomStringUtils.random(32, "0123456789ABCDEF");
+        return token;
     }
 }
